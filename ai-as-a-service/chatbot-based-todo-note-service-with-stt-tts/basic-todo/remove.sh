598e90f776eb3924612430f63e8cc73fb2b3613f
YeonwooSung <neos960518@gmail.com>
2023-01-20 17:45:47 +0900

    Add scripts for CI/CD automation

---
+++ ai-as-a-service/chatbot-based-todo-note-service-with-stt-tts/basic-todo/remove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+. ./.env
+. checkenv.sh
+
+SERVICES=(frontend todo-service resources)
+
+function remove () {
+    for SERVICE in "${SERVICES[@]}"
+    do
+        echo ----------[ removing $SERVICE ]----------
+        cd $SERVICE
+        serverless remove
+        cd ..
+    done
+}
+
+function domain () {
+  cd todo-service
+  serverless delete_domain
+  cd ..
+}
+
+domain
+
+aws s3 rm s3://${BASIC_TODO_BUCKET} --recursive
+aws s3 rm s3://${BASIC_TODO_DATA_BUCKET} --recursive
+remove
