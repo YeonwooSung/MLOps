a62fb3842f12105ba0e2c4b859c0e94b5a50bf09
YeonwooSung <neos960518@gmail.com>
2023-10-30 11:23:55 +0900

    fix: fix up runtime errors in onnx inference

--- pytorch_optimization/onnx/run_onnx_deberta_as_inference_session.py
+++ pytorch_optimization/onnx/run_onnx_deberta_as_inference_session.py
@@ -6,8 +6,13 @@ tokenizer = AutoTokenizer.from_pretrained("onnx")
 session = InferenceSession("onnx/model.onnx")
 
 # ONNX Runtime expects NumPy arrays as input
-inputs = tokenizer("Using DeBERTa with ONNX Runtime!", return_tensors="np")
+inputs = tokenizer(
+    "Using DeBERTa with ONNX Runtime!",
+    return_tensors="np",
+    return_token_type_ids=False
+)
+
 outputs = session.run(
-    output_names=["last_hidden_state"],
+    output_names=["logits"],
     input_feed=dict(inputs)
 )
