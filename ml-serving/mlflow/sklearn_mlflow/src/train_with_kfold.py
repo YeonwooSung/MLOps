704dbe7935ecf26cf10596f2b94840a0a65d9c14
YeonwooSung <neos960518@gmail.com>
2024-01-01 19:39:48 +0900

    refactor: remove unused import

--- ml-serving/mlflow/sklearn_mlflow/src/train_with_kfold.py
+++ ml-serving/mlflow/sklearn_mlflow/src/train_with_kfold.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
 
-import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
