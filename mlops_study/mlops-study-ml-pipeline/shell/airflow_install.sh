39dedb2d2fcffd3ef2e2a51eddeebb4791cac0a8
YeonwooSung <neos960518@gmail.com>
2024-10-09 16:01:19 +0900

    feat: Update airflow setup scripts

--- mlops_study/mlops-study-ml-pipeline/shell/airflow_install.sh
+++ mlops_study/mlops-study-ml-pipeline/shell/airflow_install.sh
@@ -1,6 +1,6 @@
 # Airflow needs a home. ~/airflow is the default, but you can put it
 # somewhere else if you prefer (optional)
-export AIRFLOW_HOME=~/airflow
+export AIRFLOW_HOME=${PWD}/airflow
 
 # Install Airflow using the constraints file
 AIRFLOW_VERSION=2.7.2
